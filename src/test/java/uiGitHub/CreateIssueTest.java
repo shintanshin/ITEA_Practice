56991d3d921c6bf1c830b59f6a96d436443427b9
Anna_S-V <shintanshin@gmail.com>
2023-03-04 12:42:14 +0200

    Replaced TestNG framework instead Junit. Added changes into the CreateIssueTest.java, BaseTest.java, BaseNpApiTest.java, AreaDescriptionTest.java and NewIssuesPage.java files. Added dataProvider method into the CreateIssueTest.java

--- src/test/java/uiGitHub/CreateIssueTest.java
+++ src/test/java/uiGitHub/CreateIssueTest.java
@@ -1,11 +1,21 @@
 package uiGitHub;
 
 import Pages.*;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class CreateIssueTest extends BaseTest {
-    @Test
-    public void createNewIssue() {
+    @DataProvider(name="Title&CommentData")
+    public Object[][] createData(){
+        return new Object[][]{
+                {"Issue for UI","Create UI tests"},
+                {"Issue for API","Create API tests"},
+                {"Issue for Java","Create Java tests"}
+
+        };
+    }
+    @Test(dataProvider = "Title&CommentData")
+    public void createNewIssue(String title, String comment) {
         LogInPage logInPage = new LogInPage(driver);
         logger.info("Starting Login process");
         logInPage.LogIn("anna.smahliivaza@gmail.com", "anna.smahliivaza@gmail.com");
@@ -17,6 +27,6 @@ public class CreateIssueTest extends BaseTest {
         issuesPage.goToNewIsPage();
         NewIssuesPage newIssuesPage = new NewIssuesPage(driver);
         logger.info("Starting create new issue process");
-        newIssuesPage.createNewIssue("New issue");
+        newIssuesPage.createNewIssue(title,comment);
     }
 }
